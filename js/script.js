32989e188847185578b4443d9c5a50d0269717b8
DerekNoguera <dereknoguera9@gmail.com>
2024-05-16 15:50:59 -0600

    Parte de la funcionalidad

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// const matriz = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+
+const matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(matriz);
+matriz [0][0] = "X"
+matriz [0][1] = "X"
+matriz [0][2] = "X"
+matriz [1][0] = "X"
+matriz [1][0] = "X"
+matriz [0][2] = "X"
+matriz [0][2] = "X"
+matriz [0][2] = "X"
+matriz [0][2] = "X"
+matriz [0][2] = "X"
+matriz [0][2] = "X"
+
+if (matriz [0][0] === "X" && matriz [0][1] === "X" && matriz [0][2] === "X") {
+    alert("xd")
+}
+
+
+
